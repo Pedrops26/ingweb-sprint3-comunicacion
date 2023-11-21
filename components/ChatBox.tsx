c9964fd111f36621759e45bc9304bcb19e9e7d3f
Pedrops26 <pedro.saldarriaga@udea.edu.co>
2023-11-21 09:30:50 -0500

    added a second chat

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -1,42 +1,98 @@
-// components/ChatBox.js
 import { useState, KeyboardEvent, ChangeEvent } from "react";
 
 interface ChatBoxProps {}
 
 const ChatBox: React.FC<ChatBoxProps> = () => {
-  const [messages, setMessages] = useState<string[]>([]); // Anotación de tipo añadida
-  const [newMessage, setNewMessage] = useState<string>(""); // Anotación de tipo añadida
+  const [messages1, setMessages1] = useState<
+    { text: string; sender: number }[]
+  >([]);
+  const [messages2, setMessages2] = useState<
+    { text: string; sender: number }[]
+  >([]);
+  const [newMessage1, setNewMessage1] = useState<string>("");
+  const [newMessage2, setNewMessage2] = useState<string>("");
 
-  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && newMessage.trim() !== "") {
-      setMessages([...messages, newMessage]);
-      setNewMessage("");
+  const handleKeyPress = (
+    e: KeyboardEvent<HTMLInputElement>,
+    sender: number
+  ) => {
+    if (e.key === "Enter") {
+      const newMessageText = sender === 1 ? newMessage1 : newMessage2;
+
+      if (newMessageText.trim() !== "") {
+        const newMessage = { text: newMessageText, sender };
+
+        if (sender === 1) {
+          setMessages1([...messages1, newMessage]);
+          setMessages2([...messages2, { text: newMessageText, sender: 1 }]);
+          setNewMessage1("");
+          setNewMessage2(newMessageText);
+        } else {
+          setMessages2([...messages2, newMessage]);
+          setMessages1([...messages1, { text: newMessageText, sender: 2 }]);
+          setNewMessage2("");
+          setNewMessage1(newMessageText);
+        }
+      }
     }
   };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setNewMessage(e.target.value);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, sender: number) => {
+    const value = e.target.value;
+    if (sender === 1) {
+      setNewMessage1(value);
+    } else {
+      setNewMessage2(value);
+    }
   };
 
   return (
-    <div className="max-w-md mx-auto mt-8 p-4 bg-gray-100 rounded-md">
-      <div className="h-64 overflow-y-auto bg-green-100 p-4 rounded-md">
-        {messages.map((message, index) => (
-          <div key={index} className="mb-2">
-            {message}
-          </div>
-        ))}
+    <div className="flex max-w-2xl mx-auto mt-8">
+      <div className="flex flex-col w-1/2 max-w-md p-4 bg-gray-100 rounded-md">
+        <div className="h-64 overflow-y-auto p-4 rounded-md">
+          {messages1.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-2 text-right ${
+                message.sender === 1 ? "bg-green-200" : "bg-blue-200"
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <input
+          type="text"
+          value={newMessage1}
+          onChange={(e) => handleChange(e, 1)}
+          onKeyPress={(e) => handleKeyPress(e, 1)}
+          placeholder="Escribe un mensaje..."
+          className="mt-2 p-2 border border-gray-300 rounded-md w-full"
+        />
       </div>
-      <input
-        type="text"
-        value={newMessage}
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setNewMessage(e.target.value)
-        } // Anotación de tipo añadida
-        onKeyPress={handleKeyPress}
-        placeholder="Escribe un mensaje..."
-        className="mt-2 p-2 border border-gray-300 rounded-md w-full"
-      />
+
+      <div className="flex flex-col w-1/2 max-w-md p-4 bg-gray-100 rounded-md">
+        <div className="h-64 overflow-y-auto p-4 rounded-md">
+          {messages2.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-2 text-left ${
+                message.sender === 1 ? "bg-green-200" : "bg-blue-200"
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <input
+          type="text"
+          value={newMessage2}
+          onChange={(e) => handleChange(e, 2)}
+          onKeyPress={(e) => handleKeyPress(e, 2)}
+          placeholder="Escribe un mensaje..."
+          className="mt-2 p-2 border border-gray-300 rounded-md w-full"
+        />
+      </div>
     </div>
   );
 };
